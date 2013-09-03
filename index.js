97f0c3cb47aeef4491c7ff9336e34c9d02afe53b
Ian Groff <ian.groff@you_should_know.com>
2013-09-03 16:14:39 -0500

    making a version of the diag endpoint that blows up

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+#! /usr/bin/env node
 var express = require('express');
 var app = express();
 
+app.get('/diagnostic', function(request, response) { response.send(500); });
 app.get('*', function(request, response) {
   response.end(request.path.slice(1));
 });
+
 
 app.listen(3000);
 console.log('Listening on 3000');
