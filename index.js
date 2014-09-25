59e4db38e24b74cd6b1c92f74de02c362da8f203
Ian Groff <ian@iangroff.com>
2014-09-25 11:29:39 -0500

    listen to port

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get('/diagnostic', function(request, response) { response.send(500); });
 app.get('*', function(request, response) { response.end( VERSION + "" + request.path.slice(1)); });
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('Listening on 3000');
